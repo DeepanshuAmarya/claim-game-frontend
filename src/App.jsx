4681df22407beb2cf1fdaf734d44f36898d65d11
DeepanshuAmarya <devamarya444@gmail.com>
2025-07-14 17:13:46 +0530

    Ready

---
+++ src/App.jsx
@@ -0,0 +1,77 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+function App() {
+  const [users, setUsers] = useState([]);
+  const [newName, setNewName] = useState('');
+  const [lastClaim, setLastClaim] = useState({}); // store points per user
+
+  const fetchUsers = async () => {
+    const res = await axios.get('http://localhost:5000/users');
+    setUsers(res.data);
+  };
+
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+
+  const addUser = async () => {
+    if (!newName.trim()) return;
+    const res = await axios.post('http://localhost:5000/users', { name: newName });
+    setUsers(res.data);
+    setNewName('');
+  };
+
+  const claimPoints = async (userId) => {
+    const res = await axios.post('http://localhost:5000/claim', { userId });
+    setLastClaim({ ...lastClaim, [userId]: res.data.claimedPoints });
+    setUsers(res.data.users);
+  };
+
+  return (
+    <div className="bg-blue-200" style={{ margin: 'auto', padding: '20px' }}>
+      <h2 className="font-bold flex items-center justify-center text-3xl mb-5 p-15 bg-slate-600">User Claim Game</h2>
+
+      <div style={{ marginBottom: '20px' }}>
+        <input className="border-1"
+          type="text" 
+          placeholder="Add user name" 
+          value={newName}
+          onChange={(e) => setNewName(e.target.value)}
+        />
+        <button className="bg-cyan-700 text-white hover:bg-cyan-900 p-2 rounded-2xl cursor-pointer" onClick={addUser} style={{ marginLeft: '10px' }}>Add User</button>
+      </div>
+
+      <h3 className="font-bold my-6">Leaderboard</h3>
+      <table border="1" cellPadding="8" width="100%">
+        <thead>
+          <tr>
+            <th>Rank</th>
+            <th>Name</th>
+            <th>Total Points</th>
+            <th>Claim Points</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((u) => (
+            <tr key={u._id}>
+              <td>{u.rank}</td>
+              <td>{u.name}</td>
+              <td>{u.totalPoints}</td>
+              <td>
+                <button className="bg-orange-600 p-2 rounded-2xl m-1 text-white" onClick={() => claimPoints(u._id)}>Claim</button>
+                {lastClaim[u._id] && (
+                  <span style={{ marginLeft: '10px', color: 'green' }}>
+                    +{lastClaim[u._id]} pts
+                  </span>
+                )}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default App;
